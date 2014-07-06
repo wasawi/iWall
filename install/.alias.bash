8e309ad265dba989a5ff5ad67feb5e841552e49e
Jordi <ntnu@dhcp-057114.wlan.ntnu.no>
2014-07-06 22:04:40 +0200

    added old file to check old code

--- install/.alias.bash
+++ install/.alias.bash
@@ -4,6 +4,7 @@ alias reload=". ~/.bashrc"
 alias purgeallbuilds="rm -rf ~/library/Developer/Xcode/DerivedData/*"
 alias paths="nano /etc/paths"
 alias hosts="nano /etc/hosts"
+alias go="cd //Users/ntnu/Desktop/iWall"
 
 # app
 alias iwall="/Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender /Users/ntnu/Desktop/iWall/BlenderFiles/Hallway_01.blend -noaudio -W"
@@ -13,3 +14,6 @@ alias iwall_app="open /Users/ntnu/Desktop/iWall/BlenderFiles/Hallway_01.app"
 alias max="'/Applications/Max 6.1/Max.app/Contents/MacOS/Max' /Users/ntnu/Desktop/iWall/Max/test.maxpat"
 alias of="cd /Users/ntnu/Desktop/iWall/soft/of_v0.8.2_osx_release/apps/myApps/kinectExample"
 alias blend="open /Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender"
+
+#old apps
+alias fff="/Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender /Users/ntnu/Desktop/iWall/BlenderFiles/oldFiles/FlickFlock/OF/apps/examples/FlickFlockOSC/bin/data/_mixdown_FINAL.blend -noaudio -W"
