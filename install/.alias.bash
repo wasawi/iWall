84e6c6495b2960f640676fdadf51aa8ae21a22f3
Jordi <ntnu@iWall.local>
2014-07-11 15:29:58 +0200

    colors for terminal

--- install/.alias.bash
+++ install/.alias.bash
@@ -1,18 +1,19 @@
-alias aliases="open -e ~/.alias.bash"
+alias aliases="nano ~/.alias.bash"
 alias killProcess="fuser -k -n tcp 4567"
 alias reload=". ~/.bashrc"
 alias purgeallbuilds="rm -rf ~/library/Developer/Xcode/DerivedData/*"
 alias paths="nano /etc/paths"
 alias hosts="nano /etc/hosts"
-alias go="cd //Users/ntnu/Desktop/iWall"
+alias mc=". /usr/local/Cellar/midnight-commander/4.8.12/libexec/mc/mc-wrapper.sh"
 
 # app
-alias iwall="/Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender /Users/ntnu/Desktop/iWall/BlenderFiles/Hallway_01.blend -noaudio -W"
-alias iwall_app="open /Users/ntnu/Desktop/iWall/BlenderFiles/Hallway_01.app"
+alias hallway="/Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender /Users/ntnu/Desktop/iWall/BlenderFiles/Hallway_01.blend -noaudio -W"
+alias iwall_app="cd /Users/ntnu/Desktop/iWall/soft/of_v0.8.2_osx_release/apps/myApps/iWall_app"
+alias iwall="cd //Users/ntnu/Desktop/iWall"
 
 # apps
-alias max="'/Applications/Max 6.1/Max.app/Contents/MacOS/Max' /Users/ntnu/Desktop/iWall/Max/test.maxpat"
-alias of="cd /Users/ntnu/Desktop/iWall/soft/of_v0.8.2_osx_release/apps/myApps/kinectExample"
+alias max="/Applications/Max 6.1/Max.app/Contents/MacOS/Max /Users/ntnu/Desktop/iWall/Max/test.maxpat"
+alias of="cd /Users/ntnu/Desktop/iWall/soft/of_v0.8.2_osx_release/"
 alias blend="open /Users/ntnu/Desktop/iWall/soft/Blender/blender.app/Contents/MacOS/blender"
 
 #old apps
